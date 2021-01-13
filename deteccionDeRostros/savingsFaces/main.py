ca5467359dfac2daf29ecfbfa30acb43bba6c0e9
Héctor Robles <33700655+Hector290601@users.noreply.github.com>
2021-01-12 20:05:10 -0600

    Add files via upload

--- deteccionDeRostros/savingsFaces/main.py
+++ deteccionDeRostros/savingsFaces/main.py
@@ -1,23 +1,27 @@
 import cv2
+import time
 import os
+
+i = 510
 
 frontalFace = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 profileFace = cv2.CascadeClassifier("haarcascade_profileface.xml")
 
-def savingFaces(frame, x, y, w, h, auxFrame, key, i):
+def savingFaces(frame, x, y, w, h, auxFrame, key):
+    global i
     face = auxFrame[y:y+h, x:x+w]
     face = cv2.resize(face, (200, 200), interpolation=cv2.INTER_CUBIC)
     cv2.imshow('findedFace{}'.format(i), face)
-    if key == ord('s'):
-        cv2.imwrite('faces/face_{}.jpg'.format(i), face)
-        i += 1
+    cv2.imwrite('faces/face_{}.jpg'.format(i), face)
+    print("faces/face_{}.jpg".format(i))
+    cv2.destroyWindow('findedFace{}'.format(i))
+    i += 1
 
 def showFaces(faces, frame, key):
     auxFrame = frame.copy()
-    cont = 0
     for (x1, y1, x2, y2) in faces:
         cv2.rectangle(frame, (x1, y1), (x1+x2, y1+y2), (255, 0, 0), 2)
-        savingFaces(frame, x1, y1, x2, y2, auxFrame, key, cont)
+        savingFaces(frame, x1, y1, x2, y2, auxFrame, key)
 
 def findingFaces(frame, key):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
