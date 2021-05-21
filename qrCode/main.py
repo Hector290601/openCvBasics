dc0ed2000509ab963751bf4cffffb85bc9b8a53a
Héctor Robles <33700655+Hector290601@users.noreply.github.com>
2021-05-21 13:05:56 -0500

    qr

---
+++ qrCode/main.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import zbar
+
+def main():
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+
+
+if __name__ == '__main__':
+    cap = cv2.VideoCapture(0)
+    main()
